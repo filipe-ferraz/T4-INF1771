e2f0809b24bd553501a47b05dbc738d639095f4f
filipe-ferraz <filipeffcosta@outlook.com>
2023-07-04 19:30:32 -0300

    Add files via upload
    Função da máquina de estados criada de forma separada do getdecision e getobservation. Problemas quando a observação é "blocked".

--- GameAI.py
+++ GameAI.py
@@ -35,6 +35,8 @@ class GameAI():
     scoreDelta = 0
     stateMachine = "hunt"
     previousStateMachine = ""
+    observations = []
+    cmdCount = 0
     
 
     # <summary>
@@ -141,54 +143,7 @@ class GameAI():
     # </summary>
     # <param name="o">list of observations</param>
     def GetObservations(self, o):
-
-        #cmd = "";
-        for s in o:
-            print(s)
-        
-            if s == "breeze":
-                self.stateMachine = "its_a_trap"
-                
-            elif s == "damage" or "steps":
-                if self.energy > 25:
-                    self.stateMachine = "seek"
-                else:
-                    self.stateMachine = "fly_you_fool"
-                    
-            elif s == "hit":
-                if self.energy > 25:
-                    self.stateMachine = "destroy"
-                else:
-                    self.stateMachine = "fly_you_fool"
-                
-            elif s == "blocked":
-                self.stateMachine = "bonk"
-
-            elif s == "flash":
-                pass
-            
-            elif s == "blueLight":
-                self.stateMachine = "gather_powerup"
-
-            elif s == "redLight":
-                if self.stateMachine == "gather_ouro":
-                    self.stateMachine = "gather_anel"
-                else:
-                    self.stateMachine = "gather_ouro"
-
-            elif s == "greenLight":
-                pass
-
-            elif s == "weakLight":
-                pass
-            
-            elif "enemy" in s:
-                if self.stateMachine != "destroy":
-                    self.stateMachine = "destroy"
-                pass
-        if (not o) and (self.stateMachine != "seek" or "fly_you_fool"):
-            self.stateMachine = "hunt"
-
+        self.observations = o
 
     # <summary>
     # No observations received
@@ -202,58 +157,100 @@ class GameAI():
     # </summary>
     # <returns>command string to new decision</returns>
     def GetDecision(self):
+        print ("---\nObservations: " + str(self.observations) + "\nEnergia: " + str(self.energy) + "\nState: " + self.stateMachine + "\nCommand Buffer: " + str(self.command))
+        return self.StateMachineEngine(self.observations)
+    
+    def StateMachineEngine (self, observations):
         #retornos possiveis: virar_direita, virar_esquerda, andar, atacar, pegar_ouro, pegar_anel, pegar_powerup, andar_re
-        self.previousStateMachine = self.stateMachine
         
-        
-        if self.stateMachine == "hunt":
-            n = random.randint(0, 99)
-            if n < 10:
-                self.command.append("virar_direita")
-            elif n < 20:
-                self.command.append("virar_esquerda")
-            else:
-                self.command.append("andar")
-
-        elif self.stateMachine == "bonk":
-            n = random.randint(0, 99)
-            self.stateMachine = "hunt"
-            if n < 50:
-                self.command.append("virar_direita")
+        #definição do novo estado a partir das informações do estado atual e das observações
+        if not observations: #não há observações
+            if (self.stateMachine == "gather_ouro"
+               or self.stateMachine == "gather_anel"
+               or self.stateMachine == "gather_powerup"
+               or self.stateMachine == "seek"
+               or self.stateMachine == "destroy"):
+                self.stateMachine = "hunt"
+            elif (self.stateMachine == "bonk" or self.stateMachine == "its_a_trap"):
+                self.stateMachine = self.previousStateMachine
+            
+        elif any(substring in "breeze" for substring in observations):
+            if self.stateMachine == "hunt" or self.stateMachine == "fly_you_fool":
+                self.previousStateMachine = self.stateMachine
+                self.stateMachine = "its_a_trap"
+                
+        elif any(substring in "damage, steps" for substring in observations):
+            if self.energy > 25:
+                self.stateMachine = "seek"
                 
             else:
-                self.command.append("virar_esquerda")
-            self.command.append("andar")
+                self.stateMachine = "fly_you_fool"
+                    
+        elif any(substring in "bluelight" for substring in observations):
+            if self.stateMachine == "hunt":
+                self.stateMachine = "gather_powerup"
+                
+        elif any(substring in "redlight" for substring in observations):
+            if self.stateMachine == "hunt":
+                self.stateMachine = "gather_ouro"
+            elif self.stateMachine == "gather_ouro":
+                self.stateMachine = "gather_anel"
+                
+        elif any(substring in "blocked" for substring in observations):
+            if self.stateMachine == "hunt" or self.stateMachine == "fly_you_fool":
+                self.previousStateMachine = self.stateMachine
+                self.stateMachine = "bonk"
             
-        elif self.stateMachine == "its_a_trap":
-            if self.previousStateMachine == "its_a_trap":
-                n = random.randint(0,99)
-                self.stateMachine = "hunt"
-                if n < 50:
-                    return "virar_direita"
-                else:
-                    return "virar_esquerda"
+        elif any(substring in "hit" for substring in observations):
+            if self.energy > 25:
+                self.stateMachine = "destroy"
+                self.cmdCount = 0
             else:
-                return "andar_re"
+                self.stateMachine = "fly_you_fool"
         
-        elif self.stateMachine == "fly_you_fool":
-            pass
-        elif self.stateMachine == "seek":
-            pass
-        elif self.stateMachine == "destroy":
-            pass
-        elif self.stateMachine == "gather_powerup":
-            return "pegar_powerup"
-        elif self.stateMachine == "gather_ouro":
-            return "pegar_ouro"
-        elif self.stateMachine == "gather_anel":
-            return "pegar_anel"
-    
-        
-        if self.command != []:
-            ret = self.command[0]
-            self.command.remove(ret)
-            return ret
-        return ""
-    
- 
+        #ação do novo estado
+        if not self.command: #lista de comandos vazia
+            if self.stateMachine == "hunt":
+                n = random.randint(0, 99)
+                if n < 10:
+                    self.command.append("virar_direita")
+                elif n < 20:
+                    self.command.append("virar_esquerda")
+                else:
+                    self.command.append("andar") 
+                
+            elif self.stateMachine == "its_a_trap":
+                self.command.append("andar_re")
+                self.command.append("virar_direita") #MUDANÇA: mudar pra random?
+                
+            elif self.stateMachine == "fly_you_fool":
+                pass #FALTA: função de fuga, precisa eventualmente mudar o estado para "hunt"
+                
+            elif self.stateMachine == "seek":
+                self.command.append("atacar")
+                self.command.append("virar_direita")
+                self.command.append("atacar")
+                self.command.append("virar_direita")
+                self.command.append("atacar")
+                self.command.append("virar_direita")
+                self.command.append("atacar")
+            
+            elif self.stateMachine == "destroy":
+                self.command.append("atacar")
+            
+            elif self.stateMachine == "bonk":
+                self.command.append("virar_direita")  #MUDANÇA: mudar pra random?
+            
+            elif self.stateMachine == "gather_powerup":
+                self.command.append("pegar_powerup")
+            
+            elif self.stateMachine == "gather_ouro":
+                self.command.append("pegar_ouro")
+            
+            elif self.stateMachine == "gather_anel":
+                self.command.append("pegar_anel")
+
+        ret = self.command[0]
+        print("ret = " + ret)
+        self.command.remove(ret)
+        return ret
